aa4c4bbc1fddaca3fb7455e1db72cd30e6c0cbe6
sherzod <sheraliyevs147@gmail.com>
2026-04-21 16:29:17 +0500

    added

---
+++ app.js
@@ -0,0 +1,8 @@
+const button = document.querySelector(".btn")
+const first_player =document.querySelector(.first_player)
+const secend_player =document.querySelector(.secend_player)
+
+button.addEventListener("click",() =>{
+const first_random_number  = Math.random(Math.random()*2)
+const secend_random_number  = Math.random(Math.random()*2)
+});
