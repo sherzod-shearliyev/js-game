fdb5d4cef0b0850d40c851a9262ef0153cc64fc9
sherzod <sheraliyevs147@gmail.com>
2026-04-23 16:06:36 +0500

    added

--- app.js
+++ app.js
@@ -1,8 +1,47 @@
-const button = document.querySelector(".btn")
-const first_player =document.querySelector(.first_player)
-const secend_player =document.querySelector(.secend_player)
+// JS Codes
+const btn = document.querySelector(".btn");
+const first_player = document.querySelector('.first_player')
+const result = document.querySelector('.result')
+const second_player = document.querySelector('.second_player')
+ 
+result.textContent = "o'yin boshlanishi uchun tugmani bos!!!!!!!!!!!!!!!!!!!"
 
-button.addEventListener("click",() =>{
-const first_random_number  = Math.random(Math.random()*2)
-const secend_random_number  = Math.random(Math.random()*2)
+btn.addEventListener("click", () => {
+    const first_random_number = Math.round(Math.random() * 2);
+    const second_random_number = Math.round(Math.random() * 2);
+
+    if (first_random_number == 0) {
+        first_player.setAttribute("src", "./img/paper.jpg" )
+    }else if (first_random_number == 1) {
+        first_player.setAttribute("src", "./img/rock.jpg" )
+    } else if (first_random_number == 2) {
+        first_player.setAttribute("src", "./img/qaychi.jpg" )
+    }
+
+        if (second_random_number == 0) {
+        second_player.setAttribute("src", "./img/paper.jpg" )
+    }else if (second_random_number == 1) {
+        second_player.setAttribute("src", "./img/rock.jpg" )
+    }else if (second_random_number == 2) {
+        second_player.setAttribute("src", "./img/qaychi.jpg" )
+    }
+
+        if (first_random_number == second_random_number) {
+        result.innerHTML = "Draw"
+        result.style.color = "blue"
+    } else if (first_random_number == 0 && second_random_number == 1) {
+        result.innerHTML = "Player-1 win"
+        result.style.color = "green"
+    } else if (first_random_number == 1 && second_random_number == 2) {
+        result.innerHTML = "Player-1 win"
+        result.style.color = "green"
+    } else if (first_random_number == 2 && second_random_number == 0) {
+        result.innerHTML = "Player-1 win"
+        result.style.color = "green"
+    } else {
+        result.innerHTML = "Player-2 win"
+        result.style.color = "red"
+    }
+
+
 });
